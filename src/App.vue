5af28f3f1e7b11b013c8c49135919f1a676d95b6
LvyHuang <1903757279@qq.com>
2020-06-26 23:57:20 +0800

    news-list component

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <div style="width: 1080px;margin: 0 auto">
       <Banner :banners="banners"/>
       <Channels @chooseIdChange="handleChange"/>
+      <NewsList :news="news"/>
     </div>
 
   </div>
@@ -12,10 +13,14 @@
 <script>
   import Banner from "./components/Banner";
   import Channels from "./components/news/Channels";
+  import NewsList from "./components/news/NewsList";
+  import {getNews} from "./services/newsService";
+
   export default {
     components:{
       Banner,
-      Channels
+      Channels,
+      NewsList
     },
     data(){
       return {
@@ -24,7 +29,13 @@
           {url:require("./assets/banner/banner2.jpeg"), link:"https://zhihu.com"},
           {url:require("./assets/banner/banner3.jpeg"), link:"https://sougou.com"},
         ],
-      }
+        news:[],
+    }
+    },
+    async created() {
+      var resp = await getNews("5572a108b3cdc86cf39001cd");
+      console.log(resp);
+      this.news = resp.contentlist;
     },
     methods:{
       handleChange(newId){
